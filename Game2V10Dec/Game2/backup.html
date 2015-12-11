4d5d939f66ae70451ae3b9c9d6b6e8b6b95f3355
vivekanand <vivek8943@gmail.com>
2015-12-10 18:00:18 -0600

    Game2Dec10th Version

---
+++ Game2V10Dec/Game2/backup.html
@@ -0,0 +1,132 @@
+<html>
+<head>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="http://www.amcharts.com/lib/3/amcharts.js"></script>
+<script src="http://www.amcharts.com/lib/3/serial.js"></script>
+<script src="http://www.amcharts.com/lib/3/themes/light.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<script >
+
+
+ function draw(a,b){
+
+if(chart){
+                chart.clear();
+            }
+
+
+ var chart = new AmCharts.AmSerialChart();
+ chartData =[{"Player":"1","Score":a,"color":"#ffdf97"},{"Player":"2","Score":b,"color":"#3db8c7"}]
+        
+        var chart = new AmCharts.AmSerialChart();
+        chart.dataProvider = chartData;
+        chart.categoryField = "Player";
+       
+
+        // VALUE AXIS
+        var valueAxis = new AmCharts.ValueAxis();
+        valueAxis.axisAlpha = 0.15;
+        valueAxis.minimum = 0;
+        valueAxis.maximum = 30;
+        valueAxis.dashLength = 2;
+       
+        valueAxis.unitPosition = "right";
+        chart.addValueAxis(valueAxis);
+    
+        // CATEGORY AXIS
+        chart.categoryAxis.autoGridCount = false;
+        chart.categoryAxis.gridCount = 10;
+
+        // GRAPH
+        var graph = new AmCharts.AmGraph();
+        graph.type = "column";
+        graph.colorField = "color"
+        graph.valueField = "Score";
+        graph.fillAlphas = 0.6;
+        graph.balloonText = "[[value]]";
+        chart.addGraph(graph);
+
+        // WRITE in div
+        chart.write("chartdiv");
+ 
+}
+
+
+</script>
+<script>
+var a;
+var b;
+var chart;
+var initi=0;
+
+function initval(){
+
+a=0;
+b=0;
+
+}
+
+initval();
+
+function addval(){
+ var rand=Math.floor((Math.random() * 2)+1);
+
+ if(rand==1){
+  if(initi==0){
+
+  a=a+0;
+  initi=initi+1;
+}
+else{a=a+1;}
+}
+  else if(rand==2) {if(initi==0){
+
+  b=b+0;
+  initi=initi+1;
+}
+    
+   else{ b=b+1;}
+ 
+  }
+
+
+draw(a,b);
+
+}
+
+addval();
+
+
+  
+
+</script>
+
+<script>window.onkeypress = function(event) {
+   if (event.keyCode == 49) {
+     addval();
+   }
+}</script>
+<style type="text/css">#chartdiv {#chartdiv {
+  width   : 5000%;
+  height    : 5000px;
+  font-size : 11px;
+} 
+  width   : 100px;
+  height    : 350px;
+  font-size : 11px;
+}         </style>
+
+
+
+ </head>
+<body>
+<div id="chartdiv"style="width: 500px; height: 500px;"></div> 
+ <button  type ="button" id="buyLetter" class="btn btn-default"aria-label="Center Align">Let em Play! [ 1 ] </button>   
+<button id="answer" type="button" class="btn btn-default" aria-label="Center Align">Let me Bet ! [ 2 ]</button>
+
+</body>
+</html>
---
+++ Game2V10Dec/Game2/game.php
@@ -0,0 +1,234 @@
+<html>
+<head>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="http://www.amcharts.com/lib/3/amcharts.js"></script>
+<script src="http://www.amcharts.com/lib/3/serial.js"></script>
+<script src="http://www.amcharts.com/lib/3/themes/light.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<?php 
+
+session_start();    
+
+
+?>
+<script type="text/javascript">
+var temp=0;
+    function MyFunction() {      
+        $('#buttons2').hide();
+    }
+</script>
+<script type="text/javascript">
+      $(document).ready(function () {
+
+        MyFunction();
+    });
+</script>
+<script >
+
+
+ function draw(a,b){
+
+if(chart){
+                chart.clear();
+            }
+
+
+ var chart = new AmCharts.AmSerialChart();
+ chartData =[{"Player":"1","Score":a,"color":"#ffdf97"},{"Player":"2","Score":b,"color":"#3db8c7"}]
+        
+        var chart = new AmCharts.AmSerialChart();
+        chart.dataProvider = chartData;
+        chart.categoryField = "Player";
+         chart.rotate = true;
+       
+
+        // VALUE AXIS
+        var valueAxis = new AmCharts.ValueAxis();
+        valueAxis.axisAlpha = 0.15;
+        valueAxis.minimum = 0;
+        valueAxis.maximum = 100;
+        valueAxis.dashLength = 2;
+       
+        valueAxis.unitPosition = "right";
+        chart.addValueAxis(valueAxis);
+    
+        // CATEGORY AXIS
+        chart.categoryAxis.autoGridCount = false;
+        chart.categoryAxis.gridCount = 20;
+
+        // GRAPH
+        var graph = new AmCharts.AmGraph();
+        graph.type = "column";
+        graph.colorField = "color"
+        graph.valueField = "Score";
+        graph.fillAlphas = 0.6;
+        graph.balloonText = "[[value]]";
+        chart.addGraph(graph);
+
+        // WRITE in div
+        chart.write("chartdiv");
+ 
+}
+
+
+</script>
+<script>
+var a;
+var b;
+var chart;
+var initi=0;
+
+function callsave(){
+ 
+    $.ajax({
+          url: "/Game2/test.php",
+          type: 'POST',
+         //dataType: 'text',
+          data: {SubjectId:11,ascore:a,bscore:b,isbet:0},
+          
+            error: function(xhr) {
+              //Do Something to handle error
+              console.log('error')
+            }
+          
+          
+    }); }
+    function callsave1(){
+ 
+    $.ajax({
+          url: "/Game2/test.php",
+          type: 'POST',
+         //dataType: 'text',
+          data: {SubjectId:11,ascore:a,bscore:b,isbet:1},
+          
+            error: function(xhr) {
+              //Do Something to handle error
+              console.log('error')
+            }
+          
+          
+    }); }
+
+function initval(){
+
+a=0;
+b=0;
+
+}
+
+initval();
+
+function addval(){
+ var rand=Math.floor((Math.random() * 2)+1);
+
+ if(rand==1){
+  if(initi==0){
+
+  a=a+0;
+  initi=initi+1;
+}
+else{a=a+1;}
+}
+  else if(rand==2) {if(initi==0){
+
+  b=b+0;
+  initi=initi+1;
+}
+    
+   else{ b=b+1;}
+ 
+  }
+
+
+draw(a,b);
+
+}
+
+addval();
+
+
+  
+
+</script>
+
+  <script>$(document).keypress(function(e) {
+    if(e.which == 50) {    
+      callsave1();
+      a=0;
+      b=0;
+  $("#buttons2").hide();
+   $("#buttons1").show();
+   temp=0;
+ }
+    if(e.which == 49) {
+       if( temp==0){
+      
+     addval();
+
+   }
+
+else  {
+ 
+ 
+    callsave();
+
+
+
+// store the value of A,B and time and change the buttons to previous stage
+
+a=0;
+b=0;
+  $("#buttons2").hide();
+   $("#buttons1").show();
+   temp=0;
+
+}
+    }
+    if(e.which == 50) {
+      temp=1;   
+      
+     $("#buttons1").hide();
+
+     $("#buttons2").show();
+
+    }
+});
+  
+   
+</script>
+<style type="text/css">#chartdiv {#chartdiv {
+  width   : 5000%;
+  height    : 5000px;
+  font-size : 11px;
+} 
+  width   : 100px;
+  height    : 350px;
+  font-size : 11px;
+}         </style>
+
+
+
+ </head>
+<body >
+
+<div id="chartdiv"style="width: 1240px; height: 500px;"></div> 
+<div id="buttons1">
+ <button  type ="button" id="buyLetter" class="btn btn-default"aria-label="Center Align">Play! [ 1 ] </button>   
+<button id="answer" type="button" class="btn btn-default" aria-label="Center Align">Bet ! [ 2 ]</button>
+</div>
+<div id= "buttons2" >
+
+<button  type ="button" id="buyLetter" class="btn btn-default"aria-label="Center Align">Bet On A! [ 1 ] </button>   
+<button id="answer" type="button" class="btn btn-default" aria-label="Center Align">Bet on B! [ 2 ]</button>
+  
+  
+    
+
+</div>
+
+</body>
+</html>
